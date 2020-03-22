318379b9a4bb8d3246546f6f4aaced0934118247
Wilson Snyder <wsnyder@wsnyder.org>
2020-03-22 10:18:08 -0400

    Fix license.

--- test_regress/t/t_class1.v
+++ test_regress/t/t_class1.v
@@ -1,6 +1,6 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
-// This file ONLY is placed under The Unlicense into the Public Domain, for
+// This file ONLY is placed under the Creative Commons Public Domain, for
 // any use, without warranty, 2020 by Wilson Snyder.
 // SPDX-License-Identifier: CC0-1.0
 
